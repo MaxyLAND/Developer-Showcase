f13cf864e21533a7adb8b618f8710cb05caea926
Eric Molina <100007096.joan23@fje.edu>
2024-01-24 11:58:42 +0100

    fixed anims

--- js/main.js
+++ js/main.js
@@ -1,52 +1,4 @@
 window.onload = function() { 
-    $('#logo-l').delay(100)
-        .animate({
-            opacity: 1,
-            height: '100%',
-            width: '100%'
-        }, {
-            duration: 500,
-            easing: 'swing'
-        }
-    );
-
-    $('#logo-m').delay(200)
-        .animate({
-            opacity: 1,
-            height: '100%',
-            width: '100%'
-        }, {
-            duration: 500,
-            easing: 'swing'
-        }
-    );
-
-    $('#logo-m-bright').delay(550)
-        .animate({
-            'background-position-x': '20vw'
-        }, {
-            duration: 2500,
-            easing: 'easeOutExpo'
-        }
-    );
-
-    $('#logo-l-bright').delay(550)
-        .animate({
-            'background-position-x': '20vw'
-        }, {
-            duration: 2500,
-            easing: 'easeOutExpo'
-        }
-    );
-
-    $('#logo-logo').delay(800)
-        .animate({
-            margin: '5% 0 0 5%'
-        }, {
-            duration: 800,
-            easing: 'swing'
-        }
-    );
 
     $('#logo-text').delay(1000)
         .animate({
@@ -68,10 +20,7 @@ window.onload = function() {
             opacity: 1
         }, {
             duration: 500,
-            easing: 'swing',
-            complete: function () {
-                HeaderArrowsLoop();
-            }
+            easing: 'swing'
         }
     );
 
@@ -90,36 +39,6 @@ window.onload = function() {
         }, {
             duration: 1000,
             easing: 'easeOutExpo'
-        }
-    );
-}
-
-function HeaderArrowsLoop() {
-    HeaderArrowAnim("header-arrow-down-1")
-
-    setTimeout(function() {
-        HeaderArrowAnim("header-arrow-down-2");
-    }, 666);
-
-    setTimeout(function() {
-        HeaderArrowAnim("header-arrow-down-3");
-    }, 1333);
-}
-
-function HeaderArrowAnim(id) {
-    $(`#${id}`).css("opacity", 1);
-    $(`#${id}`).css("margin", '0% 0 50% 0');
-    $(`#${id}`).delay(0)
-        .animate({
-            margin: '50% 0 0% 0',
-            opacity: 0
-        }, {
-            duration: 2000,
-            easing: 'easeOutExpo',
-            complete: function () {
-                HeaderArrowAnim(id);
-
-            }
         }
     );
 }
