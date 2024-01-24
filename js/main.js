69de668f1099b23ef3d2c6928ff80d59b723b28f
Eric Molina <100007096.joan23@fje.edu>
2024-01-24 13:51:55 +0100

    optimized

--- js/main.js
+++ js/main.js
@@ -1,49 +1,11 @@
 window.onload = function() { 
 
-    $('#logo-text').delay(1000)
-        .animate({
-            width: '55%'
-        }, {
-            duration: 800,
-            easing: 'swing'
-        }
-    );
-
     $("#logo-text span").each(function(index) {
         $(this).delay(1400 + 80 * -index).animate({ opacity: 1 }, 800);
     });
-
-
-    $('#logo-subtext').delay(1200)
-        .animate({
-            top: '65%',
-            opacity: 1
-        }, {
-            duration: 500,
-            easing: 'swing'
-        }
-    );
-
-    $('#header-background').delay(200)
-        .animate({
-            opacity: 0
-        }, {
-            duration: 2000,
-            easing: 'swing'
-        }
-    );
-
-    $('#total-visitors').delay(2000)
-        .animate({
-            top: '3%'
-        }, {
-            duration: 1000,
-            easing: 'easeOutExpo'
-        }
-    );
 }
 
-$(window).scroll(function() {
+/*$(window).scroll(function() {
     var scrolledY = $(window).scrollTop();
     $('.background-shadow').css('background-position-y', (-scrolledY * 0.5) + 'px');
-});
+});*/
