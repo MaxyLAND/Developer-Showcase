6710483af734454c234c56c17ef8d23ef60f0aae
Eric Molina <100007096.joan23@fje.edu>
2024-02-20 13:36:15 +0100

    upgraded sections

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
+window.scrollTo(0, 0); // Scroll to the top of the page
+
+window.addEventListener('wheel', function(e) {
+    e.preventDefault();
+}, { passive: false });
+
 window.onload = function() { 
     document.getElementById("header-arrow-down").onclick = function() {
         $.scrollify.next("section");
     }
-}
-
-window.onload = function() { 
-    $.scrollify({
-        section: ".section", // Clase de las secciones
-        scrollSpeed: 800, // Velocidad de desplazamiento
-        setHeights: false, // Evita que Scrollify ajuste la altura de las secciones
-        updateHash: false, // Evita que Scrollify actualice el hash de la URL
-    });
+    setTimeout(function() {
+        $.scrollify({
+            section: ".section", // Clase de las secciones
+            scrollSpeed: 800, // Velocidad de desplazamiento
+            setHeights: false, // Evita que Scrollify ajuste la altura de las secciones
+            updateHash: false, // Evita que Scrollify actualice el hash de la URL
+        });
+    }, 3100); // Delay of 3.5 seconds
 }
