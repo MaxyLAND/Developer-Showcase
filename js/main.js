14b0c13e947481d083efa6932b051ef4a25b6222
MaxyLAND <maxylandbuzon@gmail.com>
2024-08-25 14:36:34 +0200

    more progress

--- js/main.js
+++ js/main.js
@@ -1,38 +1,33 @@
 document.addEventListener('scroll', function() {
-    const banner = document.getElementById('banner');
-    const bannerLine = document.getElementById('banner-line');
-    const bannerGradient = document.getElementById('banner-gradient');
-    const bannerGradientSmall = document.getElementById('banner-gradient-small');
-    const bannerContent = document.getElementById('banner-content');
-    const bannerLogo = document.getElementById('banner-logo');
+    const bannerLine = document.getElementsByClassName('banner-line')[0];
+    const bannerGradient = document.getElementsByClassName('banner-gradient')[0];
+    const bannerGradientSmall = document.getElementsByClassName('banner-gradient-small')[0];
+    const bannerContent = document.getElementsByClassName('banner-content')[0];
+    const bannerLogo = document.getElementsByClassName('banner-logo')[0];
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition > 250) {
-        banner.classList.add('enter-animation');
-        banner.classList.remove('exit-animation');
 
-        bannerLine.classList.add('fade-in-anim');
-        bannerLine.classList.remove('fade-out-anim');
-        bannerGradient.classList.add('fade-in-anim');
-        bannerGradient.classList.remove('fade-out-anim');
-        bannerGradientSmall.classList.add('fade-in-anim');
-        bannerGradientSmall.classList.remove('fade-out-anim');
+    if (scrollPosition > 250) {
+        bannerLine.classList.add('opacity-1');
+        bannerGradient.classList.add('opacity-1');
+        bannerGradientSmall.classList.add('opacity-1');
 
         bannerContent.classList.add('center-items-anim');
         bannerLogo.classList.add('scale-1-anim');
 
-    } else if (banner.classList.contains('enter-animation')) {
-        banner.classList.add('exit-animation');
-        banner.classList.remove('enter-animation');
-
-        bannerLine.classList.add('fade-out-anim');
-        bannerLine.classList.remove('fade-in-anim');
-        bannerGradient.classList.add('fade-out-anim');
-        bannerGradient.classList.remove('fade-in-anim');
-        bannerGradientSmall.classList.add('fade-out-anim');
-        bannerGradientSmall.classList.remove('fade-in-anim');
+    } else {
+        bannerLine.classList.remove('opacity-1');
+        bannerGradient.classList.remove('opacity-1');
+        bannerGradientSmall.classList.remove('opacity-1');
 
         bannerContent.classList.remove('center-items-anim');
         bannerLogo.classList.remove('scale-1-anim');
+    }
+
+    const header = document.getElementById('header');
+    if (scrollPosition > 150) {
+        header.classList.add('header-small');
+    } else {
+        header.classList.remove('header-small');
     }
 });
